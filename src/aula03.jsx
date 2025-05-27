964f3c40dc74b7d93d5def7c13defe6f2d300ad0
Nicolas3ds2025 <nicolas.maia.taborda@escola.pr.gov.br>
2025-05-27 07:55:16 -0300

    Add files via upload

--- src/aula03.jsx
+++ src/aula03.jsx
@@ -1,33 +1,51 @@
 import React, {Component} from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 class Aula03 extends  Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            backgroundColor: 'red',
+        }
+    }
+
+    changeColor = () => {
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        this.setState({backgroundColor: randomColor});
+    }
+
     render(){
           return(
-
-            <View style={ styles.container}>
-                <Text>
-                    hello word!
-                </Text>
+            <View style={[ styles.container, {backgroundColor: this.state.backgroundColor}]}>
+                <TouchableOpacity
+                style={[styles.botao, {backgroundColor: this.state.backgroundColor}]}
+                onPress={this.changeColor}
+                >
+                    <Text style={ styles.textBotao }>Botão</Text>
+                </TouchableOpacity>
             </View>
-          );
-         
+          );      
     }
 }
 
 export default Aula03;
 
  const styles = StyleSheet.create({
-
-    container:{
+    container: {
         flex: 1,
-        backgroundColor: 'greenyellow',
-        alignItems: 'center',
         justifyContent: 'center',
-        color : 'white',
+        alignItems: 'center',
+    },
 
+    botao: {
+        backgroundColor: 'blue',
+        padding: 20,
+        borderRadius: 5,
+    },
 
-
+    textBotao: {
+        color: 'white'
     }
 
  })
